5dda9bdda873c047600e41811649bb8dfa3b6199
BRp99 <124196319+BRp99@users.noreply.github.com>
2025-02-12 15:13:21 +0000

    Add SVG favicon and update AboutView content

--- blog-app/src/components/views/About.tsx
+++ blog-app/src/components/views/About.tsx
@@ -1,3 +1,7 @@
 export default function AboutView() {
-  return <div>About</div>
+  return (
+    <div className="bg-foreground">
+      <div className="max-w-5xl mx-auto py-12 font-medium">Coming soon!</div>
+    </div>
+  )
 }
--- blog-app/src/pages/_document.tsx
+++ blog-app/src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20width='24'%20height='24'%20viewBox='0%200%2024%2024'%20fill='none'%20stroke='currentColor'%20stroke-width='2'%20stroke-linecap='round'%20stroke-linejoin='round'%3E%3Cpath%20d='M2.5%2016.88a1%201%200%2001-.32-1.43l9-13.02a1%201%200%20011.64%200l9%2013.01a1%201%200%2001-.32%201.44l-8.51%204.86a2%202%200%2001-1.98%200Z'%3E%3C/path%3E%3Cpath%20d='M12%202v20'%3E%3C/path%3E%3C/svg%3E"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />

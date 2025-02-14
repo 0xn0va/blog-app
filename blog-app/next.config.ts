a1b4708e4cae4b5d0311c15fadaa590065a0356a
BRp99 <124196319+BRp99@users.noreply.github.com>
2025-02-14 19:45:02 +0000

    config Next.js for IPFS

--- blog-app/next.config.ts
+++ blog-app/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "export", // Next.js export as a static site
-  distDir: "out", //Ensures that the generated files are saved in the out/folder - important for IPFS
+  distDir: "out", //ensures that the generated files are saved in the out/folder - important for IPFS
   images: {
     unoptimized: true, //Next.js normally optimizes images dynamically- NOT work on IPFS.
   },
